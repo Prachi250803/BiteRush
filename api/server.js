8becb4d1b9b164238d1e9ca609328637e1d7ccae
APSINGEKAR Prachi <PAR8@dsone.3ds.com>
2025-04-24 15:45:34 +0530

    url param

--- api/server.js
+++ api/server.js
@@ -25,7 +25,7 @@ app.get('/api/swiggy', async (req, res) => {
   }
 });
 app.get('/api/menu/:resId', async (req, res) => {
-  const { resId } = req.params;
+  const { resId } = req.query;
   try {
     const response = await fetch(
       `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9352403&lng=77.624532&restaurantId=${resId}&catalog_qa=undefined&submitAction=ENTER`,
