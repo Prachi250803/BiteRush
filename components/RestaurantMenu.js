c5a2e1588c4933a08b67c63b3527ff2c02954209
APSINGEKAR Prachi <PAR8@dsone.3ds.com>
2025-04-27 21:32:42 +0530

    Menu api

--- components/RestaurantMenu.js
+++ components/RestaurantMenu.js
@@ -8,11 +8,8 @@ const RestaurantMenu = () => {
   const [resInfo, setResInfo] = useState("");
   const [cartItems, setCartItems] = useState([]);
   const isLocalhost = window.location.hostname === "localhost";
+  const API_BASE = process.env.NODE_ENV === "development" ? "http://localhost:3001" : "/api";
 
-  const API_BASE = isLocalhost
-    ? "http://localhost:3001"
-    : "";
-  
   useEffect(() => {
     fetchMenu();
   }, [resId]); // ✅ Adding resId as dependency to reload menu when resId changes
